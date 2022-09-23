7bdf9a20983c991a1547ce114cc947fec189eb00
Trigdev <ahorisaac.ai@gmail.com>
2022-09-23 08:17:04 +0100

    Project Development Completed, v1.0.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { SyntheticEvent, useState } from "react";
+import data from "./data";
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState<string>("0");
+  const [text, setText] = useState<Array<string>>([]);
+
+  const handleSubmit = (e: SyntheticEvent) => {
+    e.preventDefault();
+    let amount: number = parseInt(count);
+
+    if (parseInt(count) <= 0) {
+      amount = 1;
+    }
+
+    if (parseInt(count) > 8) {
+      amount = 8;
+    }
+
+    setText(data.slice(0, amount));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="section-center">
+      <h3>tired of boring lorem ipsum?</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
+      </article>
+    </section>
   );
-}
+};
 
 export default App;
